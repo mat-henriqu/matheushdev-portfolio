97913e418fb180ebfe41c84e6bf8653e6d294982
mat-henriqu <theush933@gmail.com>
2025-04-03 11:41:12 -0300

    Refactor: Atualizar link da logo na Navbar para incluir o caminho base 'matheushdev-potfolio'

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar: React.FC = () => {
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           {/* Logo */}
-          <Link to="/" className="text-2xl font-bold text-white">
+          <Link to="/matheushdev-potfolio" className="text-2xl font-bold text-white">
             <span className="text-mathdev-primary">Matheush</span>Dev
           </Link>
 
