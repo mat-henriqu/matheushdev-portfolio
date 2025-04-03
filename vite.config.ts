3455ee4a01049ff785cc8bb0f8a3e1fe4d927b12
mat-henriqu <theush933@gmail.com>
2025-04-03 10:55:36 -0300

    configurações para deploy no github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "matheushdev-potfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
